4720e134de829954a200e1b3c2c898636ed1f33b
Elbergg <tom.p.smolen@gmail.com>
2025-12-12 20:59:11 +0100

    addressed some of mateusz's concerns

--- src/algorithms/Evolutionary_Algorithm.h
+++ src/algorithms/Evolutionary_Algorithm.h
@@ -6,14 +6,12 @@
 #include <functional>
 //T will represent the individual, strategies need to be parsed into the functions below as function pointers and they require the population and parameteres, which for now are int only because maybe that will be enough
 template <typename T>
-class Evol_Algo {
-   int population_size = 0;
+class Evolutionary_Algorithm{
     using strategy = std::function<std::vector<T>(const std::vector<T>&)>;
 public:
-   virtual ~Evol_Algo() = default;
+   virtual ~Evolutionary_Algorithm() = default;
    virtual std::vector<T> generate_init_pop();
-    virtual std::vector<T> selection(strategy, std::vector<T> population, const std::vector<int>& params);
-    virtual std::vector<T> mutation(strategy,std::vector<T> population, const std::vector<int>& params);
-    virtual std::vector<T> crossover(strategy,std::vector<T> population, const std::vector<int>& params);
-    virtual std::vector<T> succsesion(strategy,std::vector<T> population, const std::vector<int>& params);
+    virtual std::vector<T> selection(strategy, std::vector<T> population, const std::vector<float>& params);
+    virtual std::vector<T> crossover(strategy,std::vector<T> population, const std::vector<float>& params);
+    virtual std::vector<T> succsesion(strategy,std::vector<T> population, const std::vector<float>& params);
 };
