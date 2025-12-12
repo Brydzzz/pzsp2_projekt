95f55c70ccd4422e49d3a8a5faf4777d2666df2f
Elbergg <tom.p.smolen@gmail.com>
2025-12-12 21:19:01 +0100

    getting ready for SPEA2 implementation

---
+++ src/algorithms/SPEA2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tomek on 12/12/25.
+//
+
+#include "SPEA2.h"
---
+++ src/algorithms/SPEA2.h
@@ -0,0 +1,14 @@
+//
+// Created by tomek on 12/12/25.
+//
+
+#pragma once
+
+#include "Evolutionary_Algorithm.h"
+
+
+class SPEA2 : public Evol_Algo {
+
+};
+
+
