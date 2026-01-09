ce9aa539000fbea1edaf1e5d72ea345e56f8d7a4
Brydzzz <mrufkazkrem@protonmail.com>
2026-01-09 17:48:24 +0100

    implemented basic `run_gen_true_pareto()`

--- src/cli/run_gen_true_pareto.py
+++ src/cli/run_gen_true_pareto.py
@@ -1,4 +1,16 @@
+import subprocess
+
+from rich import print
+
+
 def run_gen_true_pareto(graph_fname: str, intents_fname: str) -> None:
-    print("Generating true pareto front...")
-    print(f"gf: {graph_fname}, if: {intents_fname}")
-    pass
+    print("[bold yellow]Generating true pareto front...[/bold yellow]")
+    result = subprocess.run(["echo", "hello from true pareto program :)"])
+    if result.returncode != 0:
+        print(
+            f"[bold red]Error running true pareto front generation.[/bold red] Error code: {result.returncode}"
+        )
+    else:
+        print(
+            "[bold green]Success:[/bold green] true pareto front was generated. It was saved to [TODO] in 'intents' folder"
+        )
