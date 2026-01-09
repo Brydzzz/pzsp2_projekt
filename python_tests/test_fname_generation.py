d696ebd98c39c0e3960888e35d2d95e00b35baf9
Brydzzz <mrufkazkrem@protonmail.com>
2026-01-09 17:25:57 +0100

    filename generation for algorithm compare

--- python_tests/test_fname_generation.py
+++ python_tests/test_fname_generation.py
@@ -1,17 +1,47 @@
 from datetime import datetime
-from src.cli.folder_and_fnames import generate_true_pareto_fname
+
+from src.cli.folder_and_fnames import (
+    generate_alg_comp_plots_fname,
+    generate_alg_comp_results_fname,
+    generate_true_pareto_fname,
+)
 
 
 class TestFilenamesGeneration:
     class MockDatetime:
         @classmethod
         def now(cls):
-            return datetime(2026, 1, 8, 20, 45)
+            return datetime(2026, 1, 8, 20, 45, 31)
 
     def test_generate_true_pareto_front_fname(self, monkeypatch):
         monkeypatch.setattr("src.cli.folder_and_fnames.datetime", self.MockDatetime)
         g_fname = "poland_big"
         i_fname = "poland_big_intent_10"
         result = generate_true_pareto_fname(g_fname, i_fname)
-        expected = "poland_big__poland_big_intent_10__pf__20260108_2045.csv"
+        expected = "poland_big__poland_big_intent_10__pf__20260108_204531.csv"
         assert result == expected
+
+    def test_generate_alg_comp_results_fname(self, monkeypatch):
+        monkeypatch.setattr("src.cli.folder_and_fnames.datetime", self.MockDatetime)
+        g_fname = "poland_big"
+        i_fname = "poland_big_intent_10"
+        iters = 25
+        results = generate_alg_comp_results_fname(g_fname, i_fname, iters)
+        expected = (
+            "poland_big__poland_big_intent_10__25it__alg_compare__20260108_204531.csv"
+        )
+        assert results == expected
+
+    def test_generate_alg_comp_plots_fname(self, monkeypatch):
+        monkeypatch.setattr("src.cli.folder_and_fnames.datetime", self.MockDatetime)
+        g_fname = "poland"
+        i_fname = "poland_intent_10"
+        iters = 25
+        results = generate_alg_comp_plots_fname(g_fname, i_fname, iters)
+        expected = [
+            "poland__poland_intent_10__25it__alg_compare_plot_delay__20260108_204531.csv",
+            "poland__poland_intent_10__25it__alg_compare_plot_loss__20260108_204531.csv",
+            "poland__poland_intent_10__25it__alg_compare_plot_jitter__20260108_204531.csv",
+            "poland__poland_intent_10__25it__alg_compare_plot_gdmetrics__20260108_204531.csv",
+        ]
+        assert results == expected
--- src/cli/folder_and_fnames.py
+++ src/cli/folder_and_fnames.py
@@ -11,7 +11,7 @@ def generate_true_pareto_fname(graph_fname: str, intents_fname: str) -> str:
     g_name = os.path.splitext(os.path.basename(graph_fname))[0]
     i_name = os.path.splitext(os.path.basename(intents_fname))[0]
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     return f"{g_name}__{i_name}__pf__{timestamp}.csv"
 
@@ -19,10 +19,25 @@ def generate_true_pareto_fname(graph_fname: str, intents_fname: str) -> str:
 def generate_alg_comp_results_fname(
     graph_fname: str, intents_fname: str, iterations: int
 ) -> str:
-    pass
+    g_name = os.path.splitext(os.path.basename(graph_fname))[0]
+    i_name = os.path.splitext(os.path.basename(intents_fname))[0]
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    return f"{g_name}__{i_name}__{iterations}it__alg_compare__{timestamp}.csv"
 
 
 def generate_alg_comp_plots_fname(
     graph_fname: str, intents_fname: str, iterations: int
-) -> tuple[str]:
-    pass
+) -> list[str]:
+    g_name = os.path.splitext(os.path.basename(graph_fname))[0]
+    i_name = os.path.splitext(os.path.basename(intents_fname))[0]
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    plot_names = ["delay", "loss", "jitter", "gdmetrics"]
+
+    return [
+        f"{g_name}__{i_name}__{iterations}it__alg_compare_plot_{plot_name}__{timestamp}.csv"
+        for plot_name in plot_names
+    ]
