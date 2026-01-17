3b976c076912157eb5180b2e345977daf210d870
Elbergg <tom.p.smolen@gmail.com>
2026-01-17 15:55:08 +0100

    test fix

--- python_tests/test_fname_generation.py
+++ python_tests/test_fname_generation.py
@@ -22,27 +22,35 @@ class TestFilenamesGeneration:
         assert result == expected
 
     def test_generate_alg_comp_raw_data_fname(self, monkeypatch):
-        monkeypatch.setattr("src.cli.folder_and_fnames.datetime", self.MockDatetime)
+        monkeypatch.setattr(
+            "src.cli.folder_and_fnames.datetime", self.MockDatetime
+        )
         g_fname = "poland_big"
         i_fname = "poland_big_intent_10"
         iters = 25
-        results = generate_alg_comp_raw_data_fname(g_fname, i_fname, iters)
-        expected = "poland_big__poland_big_intent_10__25it__alg_compare_raw__20260108_204531.csv"
+        runs = 100
+        mut_prob = 0.5
+        results = generate_alg_comp_raw_data_fname(
+            g_fname, i_fname, iters, runs, mut_prob
+        )
+        expected = "poland_big__poland_big_intent_10__25__100__0.5it__alg_compare_raw__20260108_204531.csv"
         assert results == expected
 
     def test_generate_alg_comp_results_fname(self, monkeypatch):
-        monkeypatch.setattr("src.cli.folder_and_fnames.datetime", self.MockDatetime)
+        monkeypatch.setattr(
+            "src.cli.folder_and_fnames.datetime", self.MockDatetime
+        )
         g_fname = "poland_big"
         i_fname = "poland_big_intent_10"
         iters = 25
+        runs = 100
+        mut_prob = 0.5
         results = generate_alg_comp_results_fname(g_fname, i_fname, iters)
-        expected = "alg_compare_results__poland_big__poland_big_intent_10__25it__20260108_204531.csv"
+        expected = "alg_compare_results__poland_big__poland_big_intent_10__25__100__0.5it__20260108_204531.csv"
         assert results == expected
 
     def test_generate_alg_comp_plots_fname(self):
-        results_fname = (
-            "alg_compare_results__poland__poland_intent_10__25it__20260108_204531.csv"
-        )
+        results_fname = "alg_compare_results__poland__poland_intent_10__25it__20260108_204531.csv"
         results = generate_alg_comp_plots_fname(results_fname)
         expected = [
             "alg_compare_plot_objectives__poland__poland_intent_10__25it__20260108_204531.png",
